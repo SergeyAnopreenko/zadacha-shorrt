42bb092366a8912d6330a023378a1b45280b228f
SergeyAnopreenko <154093984+SergeyAnopreenko@users.noreply.github.com>
2024-01-03 20:58:03 +0200

    Add project files.

---
+++ zadacha shorrt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+string text = "bcvfgdh345aaaaaaafr;";
+Dictionary<char, int> map = new();
+foreach (char c in text)
+{
+    if (map.ContainsKey(c))
+        map[c]++;
+    else
+        map[c] = 1;
+}
+
+foreach (var pair in map)
+{
+    if (Char.IsAsciiLetter(pair.Key))
+        Console.WriteLine($"{pair.Key} : {pair.Value}");
+}
+
